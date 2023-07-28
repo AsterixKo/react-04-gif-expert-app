b3fa513d9a56d969b70a10fd4d6a20ac715c7ae3
Javier Moneo <jvr.moneo@gmail.com>
2023-07-28 12:30:44 +0200

    First commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const GifExpertApp = () => {
+  // console.log(import.meta.env.VITE_GIPHY_API_KEY);
+  return (
+    <>
+      <h1>GifExpertApp</h1>
+    </>
+  );
+};
