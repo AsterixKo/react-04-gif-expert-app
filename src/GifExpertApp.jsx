0218003bbcc62ca1604823e80c5653c0e8b86991
Javier Moneo <jvr.moneo@gmail.com>
2023-07-28 18:24:56 +0200

    Creando una lista de categorias

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import { useState } from 'react';
 
 export const GifExpertApp = () => {
   // console.log(import.meta.env.VITE_GIPHY_API_KEY);
+
+  const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+
+  console.log(categories);
   return (
     <>
       <h1>GifExpertApp</h1>
+
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+        <li></li>
+      </ol>
     </>
   );
 };
