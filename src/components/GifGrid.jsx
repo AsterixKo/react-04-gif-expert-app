5781295df295978853fd7558761fad423fe7c54a
Javier Moneo <jvr.moneo@gmail.com>
2023-07-28 20:26:08 +0200

    FetchApi obtener las imagenes deseadas

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,10 +1,11 @@
+import { getGifs } from '../helpers/getGifs';
 
-
-export const GifGrid = ({category}) => {
+export const GifGrid = ({ category }) => {
+  getGifs(category);
 
   return (
     <>
-    <h3>{category}</h3>
+      <h3>{category}</h3>
     </>
-  )
-}
+  );
+};
