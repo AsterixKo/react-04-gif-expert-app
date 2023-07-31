9109393803c22a52d62e10850211fb2d555302a5
Javier Moneo <jvr.moneo@gmail.com>
2023-07-31 20:49:04 +0200

    Pruebas del componente gifGridItem parte 2

--- tests/components/GifItem.test.jsx
+++ tests/components/GifItem.test.jsx
@@ -11,9 +11,17 @@ describe("Pruebas en <GifItem/>", () => {
     expect(container).toMatchSnapshot();
   });
 
-  test('Debe de mostrar la imagen con el URL y el ALT indicado', () => { 
+  test("Debe de mostrar la imagen con el URL y el ALT indicado", () => {
     const { container } = render(<GifItem title={title} url={url} />);
 
-    // screen.
-   })
+    // expect(screen.getByRole("img").src).toBe(url);
+    // expect(screen.getByRole("img").alt).toBe(title);
+    const { src, alt } = screen.getByRole("img");
+    expect(src).toBe(url);
+    expect(alt).toBe(alt);
+  });
+  test("debe de mostrar el titulo en el componente", () => {
+    render(<GifItem title={title} url={url} />);
+    expect(screen.getByText(title)).toBeTruthy();
+  });
 });
