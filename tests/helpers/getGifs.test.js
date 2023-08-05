7f31f34367e8340e523e1d279d09403b1385e2ca
Javier Moneo <jvr.moneo@gmail.com>
2023-08-05 20:21:43 +0200

    Pruebas en el helper getGifs

--- tests/helpers/getGifs.test.js
+++ tests/helpers/getGifs.test.js
@@ -1,10 +1,14 @@
 import { getGifs } from "../../src/helpers/getGifs";
 
-// const { getGifs } = require("../../src/helpers/getGifs");
-
 describe("Pruebas en getGifs", () => {
   test("debe e retornar un arreglo de gifs", async () => {
     const gifs = await getGifs("One Punch");
-    console.log(gifs);
+    // console.log(gifs);
+    expect(gifs.length).toBeGreaterThan(0);
+    expect(gifs[0]).toEqual({
+      id: expect.any(String),
+      title: expect.any(String),
+      url: expect.any(String),
+    });
   });
 });
